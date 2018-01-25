cbc0e37946a2036cbf574a826bc91e433b0eef86
Yateesh <yateesh0988@gmail.com>
2018-01-25 12:51:39 +0530

    copy files

---
+++ Latest_update/Demo.java
@@ -0,0 +1,15 @@
+public class Demo
+{
+public static void main(String args[]){
+
+System.out.println("Jenkins is used for CI and very good");
+System.out.println("Git hub is open source & freeware");
+System.out.println("Good morning..Have a grea weekend");
+System.out.println("Push data to GitHub repository");
+for(int i=0;i<=10;i++)
+{
+	System.out.println("Jenkins");
+	i++;
+}
+}
+}
